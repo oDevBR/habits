09c0055ea4e53dab94327962ba3c2e9951bed844
oDevBR <33878228+oDevBR@users.noreply.github.com>
2023-01-19 17:52:12 -0300

    Update db models and seed

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,15 +1,13 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Form, Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
+import { Form, Link, useLoaderData } from "@remix-run/react";
 
 import { requireUserId } from "~/session.server";
 import { useUser } from "~/utils";
-import { getNoteListItems } from "~/models/note.server";
 
 export async function loader({ request }: LoaderArgs) {
   const userId = await requireUserId(request);
-  const noteListItems = await getNoteListItems({ userId });
-  return json({ noteListItems });
+  return json({ userId });
 }
 
 export default function NotesPage() {
@@ -20,7 +18,7 @@ export default function NotesPage() {
     <div className="flex h-full min-h-screen flex-col">
       <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
         <h1 className="text-3xl font-bold">
-          <Link to=".">Notes</Link>
+          <Link to=".">Home</Link>
         </h1>
         <p>{user.email}</p>
         <Form action="/logout" method="post">
@@ -33,38 +31,7 @@ export default function NotesPage() {
         </Form>
       </header>
 
-      <main className="flex h-full bg-white">
-        <div className="h-full w-80 border-r bg-gray-50">
-          <Link to="new" className="block p-4 text-xl text-blue-500">
-            + New Note
-          </Link>
-
-          <hr />
-
-          {data.noteListItems.length === 0 ? (
-            <p className="p-4">No notes yet</p>
-          ) : (
-            <ol>
-              {data.noteListItems.map((note) => (
-                <li key={note.id}>
-                  <NavLink
-                    className={({ isActive }) =>
-                      `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
-                    }
-                    to={note.id}
-                  >
-                    📝 {note.title}
-                  </NavLink>
-                </li>
-              ))}
-            </ol>
-          )}
-        </div>
-
-        <div className="flex-1 p-6">
-          <Outlet />
-        </div>
-      </main>
+      <div className="flex h-full bg-white">{`Hello World ${data.userId}`}</div>
     </div>
   );
 }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -24,21 +24,111 @@ async function seed() {
     },
   });
 
-  await prisma.note.create({
-    data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
+  const firstHabitId = "0730ffac-d039-4194-9571-01aa2aa0efbd";
+  const secondHabitId = "00880d75-a933-4fef-94ab-e05744435297";
+  const thirdHabitId = "fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00";
 
-  await prisma.note.create({
-    data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
-    },
-  });
+  await Promise.all([
+    prisma.habit.create({
+      data: {
+        id: firstHabitId,
+        title: "Beber 2L água",
+        user_id: user.id,
+        weekDays: {
+          create: [{ week_day: 1 }, { week_day: 2 }, { week_day: 3 }],
+        },
+      },
+    }),
+
+    prisma.habit.create({
+      data: {
+        id: secondHabitId,
+        title: "Exercitar",
+        user_id: user.id,
+        weekDays: {
+          create: [{ week_day: 3 }, { week_day: 4 }, { week_day: 5 }],
+        },
+      },
+    }),
+
+    prisma.habit.create({
+      data: {
+        id: thirdHabitId,
+        title: "Dormir 8h",
+        user_id: user.id,
+        weekDays: {
+          create: [
+            { week_day: 1 },
+            { week_day: 2 },
+            { week_day: 3 },
+            { week_day: 4 },
+            { week_day: 5 },
+          ],
+        },
+      },
+    }),
+  ]);
+
+  await Promise.all([
+    /**
+     * Habits (Complete/Available): 1/1
+     */
+    prisma.day.create({
+      data: {
+        /** Monday */
+        date: new Date("2023-01-02T03:00:00.000z"),
+        dayHabits: {
+          create: {
+            habit_id: firstHabitId,
+          },
+        },
+      },
+    }),
+
+    /**
+     * Habits (Complete/Available): 1/1
+     */
+    prisma.day.create({
+      data: {
+        /** Friday */
+        date: new Date("2023-01-06T03:00:00.000z"),
+        dayHabits: {
+          create: {
+            habit_id: firstHabitId,
+          },
+        },
+      },
+    }),
+
+    /**
+     * Habits (Complete/Available): 2/2
+     */
+    prisma.day.create({
+      data: {
+        /** Wednesday */
+        date: new Date("2023-01-04T03:00:00.000z"),
+        dayHabits: {
+          create: [{ habit_id: firstHabitId }, { habit_id: secondHabitId }],
+        },
+      },
+    }),
+  ]);
+
+  // await prisma.note.create({
+  //   data: {
+  //     title: "My first note",
+  //     body: "Hello, world!",
+  //     userId: user.id,
+  //   },
+  // });
+
+  // await prisma.note.create({
+  //   data: {
+  //     title: "My second note",
+  //     body: "Hello, world!",
+  //     userId: user.id,
+  //   },
+  // });
 
   console.log(`Database has been seeded. 🌱`);
 }
